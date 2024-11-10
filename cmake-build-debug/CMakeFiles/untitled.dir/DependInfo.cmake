0d511920282fd89a635a28828d57f7254fd6d096
javier <javier.alonsop9@gmail.com>
2024-11-10 17:30:36 +0100

    Separar parte 1 de la 2 (la 2 sigue sin funcionar y está usando funciones de la 1 para que no explote el programa)

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/javie/CLionProjects/PL1_Estruc.Datos/Cola.cpp" "CMakeFiles/untitled.dir/Cola.cpp.obj" "gcc" "CMakeFiles/untitled.dir/Cola.cpp.obj.d"
+  "C:/Users/javie/CLionProjects/PL1_Estruc.Datos/Lista.cpp" "CMakeFiles/untitled.dir/Lista.cpp.obj" "gcc" "CMakeFiles/untitled.dir/Lista.cpp.obj.d"
+  "C:/Users/javie/CLionProjects/PL1_Estruc.Datos/NodoCola.cpp" "CMakeFiles/untitled.dir/NodoCola.cpp.obj" "gcc" "CMakeFiles/untitled.dir/NodoCola.cpp.obj.d"
+  "C:/Users/javie/CLionProjects/PL1_Estruc.Datos/NodoLista.cpp" "CMakeFiles/untitled.dir/NodoLista.cpp.obj" "gcc" "CMakeFiles/untitled.dir/NodoLista.cpp.obj.d"
+  "C:/Users/javie/CLionProjects/PL1_Estruc.Datos/NodoPila.cpp" "CMakeFiles/untitled.dir/NodoPila.cpp.obj" "gcc" "CMakeFiles/untitled.dir/NodoPila.cpp.obj.d"
+  "C:/Users/javie/CLionProjects/PL1_Estruc.Datos/Persona.cpp" "CMakeFiles/untitled.dir/Persona.cpp.obj" "gcc" "CMakeFiles/untitled.dir/Persona.cpp.obj.d"
+  "C:/Users/javie/CLionProjects/PL1_Estruc.Datos/Pila.cpp" "CMakeFiles/untitled.dir/Pila.cpp.obj" "gcc" "CMakeFiles/untitled.dir/Pila.cpp.obj.d"
+  "C:/Users/javie/CLionProjects/PL1_Estruc.Datos/Simulacion.cpp" "CMakeFiles/untitled.dir/Simulacion.cpp.obj" "gcc" "CMakeFiles/untitled.dir/Simulacion.cpp.obj.d"
+  "C:/Users/javie/CLionProjects/PL1_Estruc.Datos/SimulacionLista.cpp" "CMakeFiles/untitled.dir/SimulacionLista.cpp.obj" "gcc" "CMakeFiles/untitled.dir/SimulacionLista.cpp.obj.d"
+  "C:/Users/javie/CLionProjects/PL1_Estruc.Datos/main.cpp" "CMakeFiles/untitled.dir/main.cpp.obj" "gcc" "CMakeFiles/untitled.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/Cola.cpp.obj"
+  "CMakeFiles/untitled.dir/Cola.cpp.obj.d"
+  "CMakeFiles/untitled.dir/Lista.cpp.obj"
+  "CMakeFiles/untitled.dir/Lista.cpp.obj.d"
+  "CMakeFiles/untitled.dir/NodoCola.cpp.obj"
+  "CMakeFiles/untitled.dir/NodoCola.cpp.obj.d"
+  "CMakeFiles/untitled.dir/NodoLista.cpp.obj"
+  "CMakeFiles/untitled.dir/NodoLista.cpp.obj.d"
+  "CMakeFiles/untitled.dir/NodoPila.cpp.obj"
+  "CMakeFiles/untitled.dir/NodoPila.cpp.obj.d"
+  "CMakeFiles/untitled.dir/Persona.cpp.obj"
+  "CMakeFiles/untitled.dir/Persona.cpp.obj.d"
+  "CMakeFiles/untitled.dir/Pila.cpp.obj"
+  "CMakeFiles/untitled.dir/Pila.cpp.obj.d"
+  "CMakeFiles/untitled.dir/Simulacion.cpp.obj"
+  "CMakeFiles/untitled.dir/Simulacion.cpp.obj.d"
+  "CMakeFiles/untitled.dir/SimulacionLista.cpp.obj"
+  "CMakeFiles/untitled.dir/SimulacionLista.cpp.obj.d"
+  "CMakeFiles/untitled.dir/main.cpp.obj"
+  "CMakeFiles/untitled.dir/main.cpp.obj.d"
+  "libuntitled.dll.a"
+  "untitled.exe"
+  "untitled.exe.manifest"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
